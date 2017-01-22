9cb15cff2a5b543786a47516c2d12eeca84c363a
yangfei <yangfei86@vip.qq.com>
2017-01-23 06:06:01 +0800

    增加使用范例

--- test.go
+++ test.go
@@ -10,12 +10,15 @@ func main() {
 	sh_res, sh_out := base.ShExec(".", "pwd")
 	fmt.Println("res: ", sh_res)
 	fmt.Println("out: ", sh_out)
-	fmt.Println("this is shell test, end!")
+	fmt.Println("this is shell test, end!\n")
 
 	str_res, str_err := base.StrToInt64("222")
 	if str_err != nil {
-		fmt.Println(str_err.Error())
+		fmt.Println(str_err.Error() + "\n")
 	} else {
-		fmt.Println(str_res)
+		fmt.Println(str_res, "\n")
 	}
+
+	fmt.Println(base.ErrorCustom("自定义错误信息").Error())
+
 }
