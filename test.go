5af5569894af941979840c681916f947068fcd70
yangfei <yangfei86@vip.qq.com>
2017-01-23 05:11:23 +0800

    测试范例

--- test.go
+++ test.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"gotools/base"
 	"fmt"
+	"gotools/base"
 )
 
 func main() {
 	fmt.Println("this is shell test, begin!\n")
-	res, out := base.ShExec(".", "pwd")
-	fmt.Println("res: ", res)
-	fmt.Println("out: ", out)
+	sh_res, sh_out := base.ShExec(".", "pwd")
+	fmt.Println("res: ", sh_res)
+	fmt.Println("out: ", sh_out)
 	fmt.Println("this is shell test, end!")
-	
-	
-	
+
+	str_res, str_err := base.StrToInt64("222")
+	if str_err != nil {
+		fmt.Println(str_err.Error())
+	} else {
+		fmt.Println(str_res)
+	}
 }
