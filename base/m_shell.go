9bfb6763b0f1075b89ce1d1050e4f30bc9ea35fe
yangfei <yangfei86@vip.qq.com>
2017-01-23 02:20:03 +0800

    增加函数的使用说明

--- base/m_shell.go
+++ base/m_shell.go
@@ -18,6 +18,17 @@ func ShExec(path, cmd string, args ...string) (string, string) {
 }
 
 /*
+注意：对于下面这种情况的实现，可能无法准确判断执行是否成功，需要主动提供判断结果，例如查询sql进行匹配。
+sqlplus sys/xxxx <<EOF
+sql info
+EOF
+
+针对正常的shell命令操作，基本上测试下来是没有任何问题的。
+
+还有就是返回值，这里因为涉及到输出，所以没有使用错误类型的返回。只返回了字符串判断。
+*/
+
+/*
 例子1：
 func main() {
 	err, out := ShExec("Unzip", "sh", "test.sh")
