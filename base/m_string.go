b129064abf7b78a7dd5ad09a4d2cb5d11295751a
yangfei <yangfei86@vip.qq.com>
2017-01-23 08:40:52 +0800

    下载模块修复一些小问题，增加字符检查功能

--- base/m_string.go
+++ base/m_string.go
@@ -1,6 +1,10 @@
 package base
 
-import "strings"
+import (
+	"fmt"
+	"github.com/deckarep/golang-set"
+	"strings"
+)
 
 //截取字符串，参数：字符串，开始下标，结束下标[包含结束下标对应的值]
 func Splitstr(str string, start int, end int) string {
@@ -11,4 +15,15 @@ func Splitstr(str string, start int, end int) string {
 //清除字符串中的空白
 func CleanSpace(str string) string {
 	return strings.Replace(str, " ", "", -1)
+}
+
+//检查字符串中是否包含某个字符
+//不支持多个字符的包含检查
+func StrContains(str, obj string) bool {
+	var a []interface{}
+	for _, v := range str {
+		a = append(a, fmt.Sprintf("%c", v))
+	}
+	class := mapset.NewSetFromSlice(a)
+	return class.Contains(obj)
 }
--- network/m_download.go
+++ network/m_download.go
@@ -2,7 +2,6 @@ package network
 
 import (
 	"fmt"
-	"github.com/astaxie/beego"
 	"gotools/base"
 	"io"
 	"math"
@@ -16,31 +15,37 @@ import (
 
 //获取下载文件的存放路径
 func GetDownloadFileAddr(download_path, download_url string) string {
-	url_slice := strings.Split(download_url, "/")
-	return download_path + url_slice[len(url_slice)-1]
+	x := base.StrContains(download_path, "/")
+	if x {
+		url_slice := strings.Split(download_url, "/")
+		return download_path + url_slice[len(url_slice)-1]
+	} else {
+		url_slice := strings.Split(download_url, "/")
+		return download_path + "/" + url_slice[len(url_slice)-1]
+	}
 }
 
 //总耗时计算,传入的值单位为秒
 func GetElapsedTimeString(all_time int) string {
 	var elapsed_time string
 	if all_time < 60 {
-		elapsed_time = strconv.Itoa(all_time) + "秒"
+		elapsed_time = base.IntToStr(all_time) + "秒"
 	} else if all_time >= 60 && all_time < 60*60 {
-		elapsed_second := strconv.Itoa(all_time%60) + "秒"
-		elapsed_time = strconv.Itoa(all_time/60) + "分" + elapsed_second
+		elapsed_second := base.IntToStr(all_time%60) + "秒"
+		elapsed_time = base.IntToStr(all_time/60) + "分" + elapsed_second
 	} else if all_time >= 3600 && all_time < 3600*24 {
-		elapsed_h := strconv.Itoa(all_time/3600) + "时"
+		elapsed_h := base.IntToStr(all_time/3600) + "时"
 		m := all_time % 3600
-		elapsed_m := strconv.Itoa(m/60) + "分"
-		elapsed_s := strconv.Itoa(m%60) + "秒"
+		elapsed_m := base.IntToStr(m/60) + "分"
+		elapsed_s := base.IntToStr(m%60) + "秒"
 		elapsed_time = elapsed_h + elapsed_m + elapsed_s
 	} else {
-		elapsed_d := strconv.Itoa(all_time/(3600*24)) + "天"
+		elapsed_d := base.IntToStr(all_time/(3600*24)) + "天"
 		h := all_time % (3600 * 24)
-		elapsed_h := strconv.Itoa(h/3600) + "时"
+		elapsed_h := base.IntToStr(h/3600) + "时"
 		m := h % 3600
-		elapsed_m := strconv.Itoa(m/60) + "分"
-		elapsed_s := strconv.Itoa(m%60) + "秒"
+		elapsed_m := base.IntToStr(m/60) + "分"
+		elapsed_s := base.IntToStr(m%60) + "秒"
 		elapsed_time = elapsed_d + elapsed_h + elapsed_m + elapsed_s
 	}
 
@@ -49,10 +54,9 @@ func GetElapsedTimeString(all_time int) string {
 
 //去程专用下载返回,传入下载地址,本地绝对路径
 func GoDownload(wg *sync.WaitGroup, download_path, download_url string) {
-	log := base.HttpSetLog()
 	res, err := http.Get(download_url)
 	if err != nil {
-		log.Error("连接远程服务器时出错!" + err.Error())
+		fmt.Println("连接远程服务器时出错!" + err.Error())
 	}
 
 	//获取下载文件的存放路径
@@ -61,7 +65,7 @@ func GoDownload(wg *sync.WaitGroup, download_path, download_url string) {
 	target_file, err := os.Create(target_addr)
 	defer target_file.Close()
 	if err != nil {
-		log.Error("创建本地文件出错:" + err.Error())
+		fmt.Println("创建本地文件出错:" + err.Error())
 	}
 	io.Copy(target_file, res.Body)
 
@@ -70,14 +74,14 @@ func GoDownload(wg *sync.WaitGroup, download_path, download_url string) {
 
 //去程专用,检查下载信息。传入下载地址,本地保存路径,开始下载时间
 func GoInfo(wg *sync.WaitGroup, download_path, download_url string, start_time time.Time) {
-	log := base.HttpSetLog()
+
 	res, err := http.Get(download_url)
 	if err != nil {
-		log.Error("连接远程服务器时出错!" + err.Error())
+		fmt.Println("连接远程服务器时出错!" + err.Error())
 	}
 
 	size, size_int := base.GetSize(res.ContentLength)
-	log.Info("远程文件的大小为: " + size)
+	fmt.Println("远程文件的大小为: " + size)
 
 	//获取下载文件的存放路径
 	target_addr := GetDownloadFileAddr(download_path, download_url)
@@ -94,13 +98,13 @@ func GoInfo(wg *sync.WaitGroup, download_path, download_url string, start_time t
 		if ps < 100 {
 			jdt = strings.Repeat("=", ps/2) + ">" + strings.Repeat(" ", 50-ps/2)
 			download_percent = strconv.Itoa(ps) + "%"
-			showstr = "\r已下载: %s/%s [%s] 已完成:%s 当前下载速度:%s/s 已耗时:%s         "
+			showstr = "\r已下载: %s/%s [%s] 已完成:%s 当前下载速度:%s 已耗时:%s         "
 			//因为还没有下载完成，所以下载速度就是上一秒和下一秒的差
 			avg_str = sd_str
 		} else {
 			jdt = strings.Repeat("=", 50)
 			download_percent = "100%"
-			showstr = "\r已下载: %s/%s [%s] 已完成:%s 平均下载速度:%s/s 总耗时:%s          \n"
+			showstr = "\r已下载: %s/%s [%s] 已完成:%s 平均下载速度:%s 总耗时:%s          \n"
 		}
 
 		//计算时长
@@ -110,11 +114,11 @@ func GoInfo(wg *sync.WaitGroup, download_path, download_url string, start_time t
 
 		//平均速度计算
 		if size_int < 1024 {
-			avg_str = strconv.Itoa(int(float32(size_int)/float32(all_time))) + "B/s"
+			avg_str = base.IntToStr(int(float32(size_int)/float32(all_time))) + "B/s"
 		} else if 1024 <= size_int && size_int < 1024*1024 {
-			avg_str = strconv.FormatFloat(float64(size_int)/1024/float64(all_time), 'f', 2, 64) + "K/s"
+			avg_str = base.Float64ToStr(float64(size_int)/1024/float64(all_time)) + "K/s"
 		} else {
-			avg_str = strconv.FormatFloat(float64(size_int)/1024/1024/float64(all_time), 'f', 2, 64) + "M/s"
+			avg_str = base.Float64ToStr(float64(size_int)/1024/1024/float64(all_time)) + "M/s"
 		}
 
 		//打印下载进度条
@@ -133,24 +137,11 @@ func GoInfo(wg *sync.WaitGroup, download_path, download_url string, start_time t
 }
 
 //功能整合,将下载代码抽象到一个函数中
-func UrlDownload(download_path, download_url string) (string, string) {
-	//载入日志配置
-	log := base.HttpSetLog()
-
+func UrlDownload(download_path, download_url string) (string, error) {
 	//清理下载目录
-	dir_path := beego.AppConfig.String("download_path")
-	err, out := base.ClearDir(dir_path)
-	if err == "error" {
-		log.Error("清理下载目录出现异常:" + out)
-		return "error", "清理下载目录出现异常:" + out
-	}
-
-	//清理解压目录
-	unzip_path := beego.AppConfig.String("unzip_path")
-	err1, out1 := base.ClearDir(unzip_path)
-	if err1 == "error" {
-		log.Error("清理解压目录出现异常:" + out1)
-		return "error", "清理解压目录出现异常:" + out1
+	_, err := base.ClearDir(download_path)
+	if err != nil {
+		return "清理下载目录出现异常:" + err.Error(), err
 	}
 
 	//记录下载开始时间
@@ -165,5 +156,5 @@ func UrlDownload(download_path, download_url string) (string, string) {
 	wg.Wait()
 
 	//全部执行完成,返回0
-	return "ok", "下载完成!"
+	return "下载完成!", nil
 }
