8284cbb66b62e5e321997f9cf0f15e87858318db
yangfei <yangfei86@vip.qq.com>
2017-03-15 13:54:56 +0800

    增加功能: 获取日期对应的星期几

--- time/m_timeFormat.go
+++ time/m_timeFormat.go
@@ -1,6 +1,9 @@
 package time
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 //完整的日期和时间
 func GetFullTime() string {
@@ -17,9 +20,24 @@ func GetTime() string {
 	return time.Now().Format("15:04:05")
 }
 
+//获取日期相对的周几
+func GetWeekDay(Year, Month, Day string) string {
+	if len(Month) == 1 {
+		Month = "0" + Month
+	}
+	if len(Day) == 1 {
+		Day = "0" + Day
+	}
+
+	DATE := fmt.Sprintf("%s-%s-%s", Year, Month, Day)
+	t, _ := time.Parse("2006-01-02", DATE)
+	return t.Weekday().String()
+}
+
 /*
 例子：
 fmt.Println(time.GetFullTime())  // 2017-02-04 18:45:53
 fmt.Println(time.GetData())      // 2017-02-04
 fmt.Println(time.GetTime())      // 18:45:53
+fmt.Println(GetWeekDay("2017", "3","1"))  //Wednesday
 */
