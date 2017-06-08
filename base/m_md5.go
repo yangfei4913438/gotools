d02816699f559e5eb0c7b03b4f3eae105d6f3f15
yangfei <yangfei86@vip.qq.com>
2017-06-08 11:43:55 +0800

    增加功能: sha256加密

--- base/m_md5.go
+++ base/m_md5.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 )
 
@@ -9,4 +10,11 @@ func StrMD5(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func StrSHA256(str string) string {
+	hash_value := sha256.New()
+	hash_value.Write([]byte(str))
+	md := hash_value.Sum(nil)
+	return hex.EncodeToString(md)
 }
