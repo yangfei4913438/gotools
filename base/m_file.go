32e1922a38f32502b6710e457c95a20f77f8d199
yangfei <yangfei86@vip.qq.com>
2017-06-20 13:59:59 +0800

    更新文件检查模块

--- base/m_file.go
+++ base/m_file.go
@@ -98,10 +98,7 @@ func GetFileSize(file_addr string) (string, int64) {
 
 //检查路径是否存在。文件和目录都可以检查。
 func CheckIsExist(addr string) bool {
-	f, err := os.Open(addr)
-	if f != nil {
-		f.Close()
-	}
+	_, err := os.Stat(addr)
 	if err != nil && os.IsNotExist(err) {
 		//路径不存在
 		return false
