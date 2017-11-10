242900158a76bcd02c871f65e689ead9c48f30eb
yangfei <yangfei86@vip.qq.com>
2017-11-10 09:46:49 +0800

    增加删除包含指定前缀redis缓存的功能

--- db/redis.go
+++ db/redis.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/astaxie/beego"
 	redis "github.com/yangfei4913438/redis-full"
+	"strings"
 	"time"
 )
 
@@ -39,4 +40,27 @@ func initRedis() {
 	} else {
 		beego.Info("Connect Redis Server(" + hosts + ") to successful!")
 	}
+}
+
+// 清理指定前缀的redis缓存
+func CleanRedis(prefixes ...string) error {
+	// 获取所有的key
+	keys, err := RedisDB.Keys()
+	if err != nil {
+		beego.Error(err)
+		return err
+	}
+
+	// 遍历这些key，删掉包含指定前缀的key
+	for _, key := range keys {
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(key, prefix) {
+				RedisDB.Del(key)
+				// 删除完成了，就不用继续检查了，换下个KEY
+				break
+			}
+		}
+	}
+
+	return nil
 }
