79f99e548175b2184be119dcb9034fc03d522bf0
yangfei <yangfei86@vip.qq.com>
2017-11-20 15:34:07 +0800

    base test

--- base_test.go
+++ base_test.go
@@ -31,3 +31,51 @@ func Test_Splitstr(t *testing.T) {
 		t.Log("split string, check ok!")
 	}
 }
+
+func Test_IntToStr(t *testing.T) {
+	x := int(1)
+	y := base.IntToStr(x)
+	if y != "1" {
+		t.Error("change int type to string, check error! right result is '1', but result is:", y)
+	} else {
+		t.Log("change int type to string, check ok!")
+	}
+}
+
+func Test_Int64ToStr(t *testing.T) {
+	x := int64(123456789)
+	y := base.Int64ToStr(x)
+	if y != "123456789" {
+		t.Error("change int64 type to string, check error! right result is '123456789', but result is:", y)
+	} else {
+		t.Log("change int64 type to string, check ok!")
+	}
+}
+
+func Test_StrToInt(t *testing.T) {
+	x := "123"
+	y, err := base.StrToInt(x)
+	if err != nil {
+		t.Error("change string type to int, found error:", err)
+	}
+
+	if y != int(123) {
+		t.Error("change string type to int, check error! right result is 123, but result is:", y)
+	} else {
+		t.Log("change string type to int, check ok!")
+	}
+}
+
+func Test_StrToInt64(t *testing.T) {
+	x := "1234567"
+	y, err := base.StrToInt64(x)
+	if err != nil {
+		t.Error("change string type to int64, found error:", err)
+	}
+
+	if y != int64(1234567) {
+		t.Error("change string type to int64, check error! right result is 1234567, but result is:", y)
+	} else {
+		t.Log("change string type to int64, check ok!")
+	}
+}
